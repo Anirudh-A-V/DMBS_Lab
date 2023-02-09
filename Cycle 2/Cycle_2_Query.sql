67502357265a2c8b0d4cb995f59ec1922d55f042
Anirudh A V <anirudh.av02@gmail.com>
2023-02-09 12:35:26 +0530

    cycle 2 initial

---
+++ Cycle 2/Cycle_2_Query.sql
@@ -0,0 +1,5 @@
+insert into faculty values(11, 'active', 'senior', '2027-05-11');
+insert into faculty values(10, 'active', 'junior', '2023-05-11');
+insert into faculty values(24, 'active', 'junior', '2030-05-11');
+insert into faculty values(23, 'on leave', 'senior','2026-05-11');
+
